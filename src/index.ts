982e38ca7249126d2a59c4f9204627800c9633ce
donmahallem <donmahallem@users.noreply.github.com>
2020-01-13 17:22:17 +0100

    save

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,26 @@
  * Source https://github.com/donmahallem/TrapezeApiClientNode
  */
 
-export * from "./vehicle-storage";
-export { TimestampedVehiclelocation } from "./timestamped-location";
+import * as jpeg from "jpeg-js";
+
+function fuzz(buf) {
+    try {
+        jpeg.decode(buf);
+    } catch (e) {
+        // Those are "valid" exceptions. we can't catch them in one line as
+        // jpeg-js doesn't export/inherit from one exception class/style.
+        if (e.message.indexOf('JPEG') !== -1 ||
+            e.message.indexOf('length octect') !== -1 ||
+            e.message.indexOf('Failed to') !== -1 ||
+            e.message.indexOf('DecoderBuffer') !== -1 ||
+            e.message.indexOf('invalid table spec') !== -1 ||
+            e.message.indexOf('SOI not found') !== -1) {
+        } else {
+            throw e;
+        }
+    }
+}
+
+module.exports = {
+    fuzz
+};
